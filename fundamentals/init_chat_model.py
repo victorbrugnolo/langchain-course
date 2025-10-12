bb06bc6c47920d94c572ec9b4e604a06932f07c1
Victor Brugnolo <brugnolovictor@gmail.com>
2025-10-12 13:52:43 -0300

    add init chat model example

---
+++ fundamentals/init_chat_model.py
@@ -0,0 +1,8 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
+answer_gemini = gemini.invoke("Hello, world!")
+print(answer_gemini.content)
