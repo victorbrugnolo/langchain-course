53cb6c6167f4343b8bc6c8557876f71e1a32d25c
Victor Brugnolo <brugnolovictor@gmail.com>
2025-10-18 18:16:07 -0300

    feat: reAct, agent using prompt hub

---
+++ agents-and-tools/agent_react_using_prompt_hub.py
@@ -0,0 +1,48 @@
+from langchain.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+from dotenv import load_dotenv
+load_dotenv()
+
+@tool("calculator", return_direct=True)
+def calculator(expression: str) -> str:
+    """Evaluate a simple mathematical expression and return the result as a string."""
+    try:
+        result = eval(expression)  # cuidado: apenas para exemplo didático
+    except Exception as e:
+        return f"Error: {e}"
+    return str(result)
+
+@tool("web_search_mock")
+def web_search_mock(query: str) -> str:
+    """Return the capital of a given country if it exists in the mock data."""
+    data = {
+        "Brazil": "Brasília",
+        "France": "Paris",
+        "Germany": "Berlin",
+        "Italy": "Rome",
+        "Spain": "Madrid",
+        "United States": "Washington, D.C."
+        
+    }
+    for country, capital in data.items():
+        if country.lower() in query.lower():
+            return f"The capital of {country} is {capital}."
+    return "I don't know the capital of that country."
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+tools = [web_search_mock]
+
+prompt = hub.pull("hwchase17/react")
+agent_chain = create_react_agent(llm, tools, prompt)
+
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent_chain, 
+    tools=tools, 
+    verbose=True, 
+    # max_iterations=5
+)
+
+print(agent_executor.invoke({"input": "What is the capital of Iran?"}))
+# print(agent_executor.invoke({"input": "How much is 10 + 10?"}))
